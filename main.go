50e40b3da063b886af66b9c1e4e59d84de4ac210
Anugopinathan <anu.gopinathan@gmail.com>
2022-09-15 12:16:42 +0530

    Hello world

--- main.go
+++ main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"fmt"
-	"stack"
 )
 
 func main() {
-	s := new(stack.Mystack)
-	for i := 1; i < 4; i++ {
-		s.Push(i)
-	}
-
-	for i := 1; i < 5; i++ {
-		p, err := s.Pop()
-		if err == nil {
-			fmt.Println(p)
-		} else {
-			fmt.Println(err)
-		}
-	}
-	//s.isempty() this is error sice is empty is not exported in Mystack
+	fmt.Println("Hello welcome to Anu's world")
 }
